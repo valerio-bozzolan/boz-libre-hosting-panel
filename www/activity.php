4cd3394cf3d7d16587f636059a0380c23dd3d6dd
Valerio Bozzolan <boz@reyboz.it>
2020-08-02 17:08:17 +0200

    Create a global audit page
    Closes T436

---
+++ www/activity.php
@@ -0,0 +1,38 @@
+<?php
+# Copyright (C) 2018, 2019, 2020 Valerio Bozzolan
+# KISS Libre Hosting Panel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * This is the homepage of your hosting panel
+ */
+
+// load framework
+require '../load.php';
+
+// spawn header
+Header::spawn();
+
+?>
+
+	<?php ActivityPanel::spawn( [
+		'query' => [
+			'limit' => 200,
+		],
+	] ) ?>
+
+<?php
+
+Footer::spawn();
